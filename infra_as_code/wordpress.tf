8c900229dfb4fca0e87453e5d39e4560de25e743
norbertLap <knez.norbert@gmail.com>
2024-05-31 15:55:20 +0200

    health check & deregistration delay adjustments

--- infra_as_code/wordpress.tf
+++ infra_as_code/wordpress.tf
@@ -143,6 +143,16 @@ resource "aws_lb_target_group" "lbTargetGroup" {
   port = 80
   protocol = "HTTP"
   vpc_id = aws_vpc.WordpressVPC.id 
+  deregistration_delay = 60
+  
+  health_check {
+    path = "/"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    matcher = "200-399"
+  }
 }
 
 # IAM role and instance profile for the webserver to provide them necessary permissions
